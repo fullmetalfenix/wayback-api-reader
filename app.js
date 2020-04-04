3527c665c6e53e334af54aa6a404d6b15d2c96ed
Jason <Jpihlk@yahoo.com>
2020-04-04 16:43:43 -0400

    set up form to return

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //JS Going here
+
+class Initial extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+      };
+    };
+      
+//  componentDidMount(){
+//}
+  
+    render(){
+          return(
+       <div id="main-container">
+            <div id="make-shift-form">
+                <input type="text" name="url" id="get-url" />
+                <button id="submit-button">Search</button>
+            </div>
+
+       </div>
+       )
+   }
+
+   
+  } // End of component
+  
+  ReactDOM.render(<Initial />, document.getElementById('app-container'));
