42fc4ed78ce15b091932704ff41576af5e65bef0
Jason <Jpihlk@yahoo.com>
2020-04-04 17:30:36 -0400

    form styling done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class Initial extends React.Component{
     render(){
           return(
        <div id="main-container">
+            <h1>Wayback <span style={{color:'#a92e33'}}>Machine</span> API</h1>
             <div id="make-shift-form">
                 <input type="text" name="url" id="get-url" />
                 <button id="submit-button">Search</button>
